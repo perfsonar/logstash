dd268d584d892c2560d43620b2b08b6e54086f19
Luan Rios <luan@merry>
2021-09-16 15:47:26 -0300

    Correct the setup of xenvironment variables file for logstash

--- scripts/install_logstash_sysconfig_debian.sh
+++ scripts/install_logstash_sysconfig_debian.sh
@@ -2,4 +2,6 @@
 
 if [ ! -e /etc/default/logstash ]; then
     ln -s /etc/perfsonar/logstash/logstash_sysconfig /etc/default/logstash
+else
+	cp /etc/perfsonar/logstash/logstash_sysconfig /etc/default/logstash
 fi
