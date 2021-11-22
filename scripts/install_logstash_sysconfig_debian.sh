b1432bba4bf2df0b032599f8e0449611d11f4a59
Luan Rios <luancgrios@gmail.com>
2021-11-22 10:36:38 -0300

    Small identation fix

--- scripts/install_logstash_sysconfig_debian.sh
+++ scripts/install_logstash_sysconfig_debian.sh
@@ -3,5 +3,5 @@
 if [ ! -e /etc/default/logstash ]; then
     ln -s /etc/perfsonar/logstash/logstash_sysconfig /etc/default/logstash
 else
-	cp /etc/perfsonar/logstash/logstash_sysconfig /etc/default/logstash
+    cp /etc/perfsonar/logstash/logstash_sysconfig /etc/default/logstash
 fi
