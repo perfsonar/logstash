f3702f3ee316f699a06f73bfe7ba4173d27253d3
Daniel Neto <daniel.jsilvan@gmail.com>
2022-02-10 00:09:13 -0300

    Improving configuration script

--- scripts/install_logstash_sysconfig.sh
+++ scripts/install_logstash_sysconfig.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-if [ ! -e /etc/sysconfig/logstash ]; then
-    ln -s /etc/perfsonar/logstash/logstash_sysconfig /etc/sysconfig/logstash
+if command -v lsb_release &> /dev/null; then 
+    OS=$(lsb_release -si)
+elif [ -f /etc/os-release ]; then
+    OS=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+elif [ -d "/etc/sysconfig" ]; then
+    OS="CentOS"
+elif [ -d "/etc/default" ]; then
+    OS="Debian"
+else
+    OS="Unknown"
+fi
+
+if [[ $OS == *"CentOS"* ]]; then
+    if [ ! -e /etc/sysconfig/logstash ]; then
+        ln -s /etc/perfsonar/logstash/logstash_sysconfig /etc/sysconfig/logstash
+    fi
+elif [[ $OS == *"Debian"* ]] || [[ $OS == *"Ubuntu"* ]]; then
+    if [ ! -e /etc/default/logstash ]; then
+        ln -s /etc/perfsonar/logstash/logstash_sysconfig /etc/default/logstash
+    fi
+else
+    echo "$0 - [ERROR]: Unknown operating system"
 fi
