00c9edcc26e1aa09e066f85357aadd084a8c2dfb
Andy Lake <andy@es.net>
2023-08-28 19:53:10 -0700

    Adding previous check to better location

--- perfsonar-logstash/perfsonar-logstash/ruby/prometheus_parse.rb
+++ perfsonar-logstash/perfsonar-logstash/ruby/prometheus_parse.rb
@@ -20,6 +20,9 @@ end
 
 def _filter(event)
     prom_data = event.get("[message]")
+    if prom_data.nil? then
+        return []
+    end
 
     ####
     # The following will need to be configured externally
@@ -38,9 +41,6 @@ def _filter(event)
     record_type = event.get("[type]")
     rate_key_prefix = "#{meta_id}::#{record_type}"
     curr_time = event.get("@timestamp").to_i
-    if prom_data.nil? then
-        return []
-    end
     prom_data.each_line do |prom_line|
         if type_matches = prom_line.match(/^\#\s*TYPE\s+([a-zA-Z_:][a-zA-Z0-9_:]*)\s+(\w+)\s*$/) then
             #we don't actually use this, but seems useful when support for histograms gets added
