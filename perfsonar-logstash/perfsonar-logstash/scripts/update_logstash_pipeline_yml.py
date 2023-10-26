d792e4d12e85e9de7d391273eafcab954fded2d8
Andy Lake <andy@es.net>
2023-10-26 16:33:52 -0400

    Small fix for logstash 8 pipeline

--- perfsonar-logstash/perfsonar-logstash/scripts/update_logstash_pipeline_yml.py
+++ perfsonar-logstash/perfsonar-logstash/scripts/update_logstash_pipeline_yml.py
@@ -11,16 +11,24 @@ with open('/etc/logstash/pipelines.yml') as file:
     pipeline_exists = False
 
 #look for pscheduler pipeline
+write_file = False
 for pipeline in pipelines_yml:
     if pipeline is not None and pipeline.get("pipeline.id", None) == "pscheduler":
-            pipeline_exists = True
-            break
+        if pipeline.get("pipeline.ecs_compatibility", "") != "disabled":
+            pipeline["pipeline.ecs_compatibility"] = "disabled"
+            write_file = True
+        pipeline_exists = True
+        break
 
 #if not exists, then add it and output to file
 if not pipeline_exists:
+    write_file = True
     pipelines_yml.append({
         "pipeline.id": "pscheduler",
-        "path.config": "/usr/lib/perfsonar/logstash/pipeline/*.conf"
+        "path.config": "/usr/lib/perfsonar/logstash/pipeline/*.conf",
+        "pipeline.ecs_compatibility": "disabled"
     })
+
+if write_file:
     with open('/etc/logstash/pipelines.yml', 'w') as file:
         file.write(yaml.dump(pipelines_yml, default_flow_style=False))
